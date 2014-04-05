4316aa4935475ef2503821b8cb155a6c75164265
PolyakovD <polyakovdmi93@gmail.com>
2014-04-05 12:54:03 +0400

    build and  tests

--- test/TestGameMaharajah.java
+++ test/TestGameMaharajah.java
@@ -6,41 +6,99 @@
 
 /**
  *
- * @author Денис
  */
+import org.junit.Before;
+import org.junit.BeforeClass;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-/*
-import game.Game;
-import board.Board;
-import move.Move;
-import piece.Piece;
-import piece.PieceType;
-import player.Player;
-import player.ParameterProcessor;
-import board.Position;
-*/
+//
+import model.Game;
+import IO.RulesParser;
+import IO.TextViewer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import model.coord.Entry;
+import model.coord.Position;
+import model.moves.Move;
+import model.set.Board;
+import model.set.Piece;
+import model.set.PieceType;
+import model.set.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.Vector;
 
 @RunWith(JUnit4.class)
 public class TestGameMaharajah {
-    //private Game game;
+    private Game game;
 	
     @Test
-    public void testMove() {
-		//game = RulesParser.parse("./rules/Maharajah.xml");
-        int x = 1;
+    public void test01Move() {
+		game = RulesParser.parse("./rules/Maharajah.xml");
+        int x = 4;
         int y = 7;
-        int xDest = 1;
+        int xDest = 4;
         int yDest = 5;
         
-        //boolean result = game.move(new Position(x, y), new Position(xDest, yDest));
-        //boolean expected = false;
-        assertTrue(true);
+        boolean result = game.move(new Position(x, y), new Position(xDest, yDest));
+        boolean expected = true;
+		assertEquals(expected, result);
+		System.out.print("test01 passed");
+    } 
+	
+	@Test
+	public void test02Move() {
+		game = RulesParser.parse("./rules/Maharajah.xml");
+        int x = 4;
+        int y = 7;
+        int xDest = 4;
+        int yDest = 6;
+        
+        boolean result = game.move(new Position(x, y), new Position(xDest, yDest));
+        boolean expected = true;
+        //assertTrue("Error", expected == result);
+		assertEquals(expected, result);
+		System.out.print(" test02 passed");
+    } 
+	
+	@Test
+	public void test03Move() {
+		game = RulesParser.parse("./rules/Maharajah.xml");
+        int x = 2;
+        int y = 7;
+        int xDest = 2;
+        int yDest = 6;
+        
+        boolean result = game.move(new Position(x, y), new Position(xDest, yDest));
+        boolean expected = true;
+        //assertTrue("Error", expected == result);
+		if(result != expected) {
+			fail("Not equals in 03");
+		}
+		System.out.print(" test03 passed");
+    } 
+	
+	@Test
+	public void test04Move() {
+		game = RulesParser.parse("./rules/Maharajah.xml");
+        int x = 5;
+        int y = 1;
+        int xDest = 5;
+        int yDest = 2;
+        
+        boolean result = game.move(new Position(x, y), new Position(xDest, yDest));
+        boolean expected = true;
+        //assertTrue("Error", expected == result);
+		if(result != expected) {
+			fail("Not equals in 04");
+		}
+		System.out.print(" test04 passed");
     } 
 }
