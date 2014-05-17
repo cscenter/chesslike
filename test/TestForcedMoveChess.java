91582dd76325e4d75c0a8f8bf76253d8ad282de0
PolyakovD <polyakovdmi93@gmail.com>
2014-05-17 11:55:12 +0400

    tests

--- test/TestForcedMoveChess.java
+++ test/TestForcedMoveChess.java
@@ -52,129 +52,36 @@ public class TestForcedMoveChess {
 			int ySize = sc.nextInt();
 			//
 			int [][] newField = new int[xSize][ySize];
-			for (int y = 0; y < ySize; ++y) {
+			for (int y = ySize - 1; y > -1; --y) {
 				for (int x = 0; x < xSize; ++x) {
 					newField[x][y] = sc.nextInt();
 				}
 			}
 			//
 			int [][] playersId = new int[xSize][ySize];
-			for (int y = 0; y < ySize; ++y) {
+			for (int y = ySize - 1; y > -1; --y) {
 				for (int x = 0; x < xSize; ++x) {
 					playersId[x][y] = sc.nextInt();
 				}
 			}
+			//
+			int [][] specialMovesId = new int[xSize][ySize];
+			for (int y = ySize - 1; y > -1; --y) {
+				for (int x = 0; x < xSize; ++x) {
+					specialMovesId[x][y] = sc.nextInt();
+				}
+			}
+			
+			int ownTurn = sc.nextInt();
+
 			sc.close();
 			//
 			game = RulesParser.parse("./rules/Chess.xml");
-			game.forcedMove(newField, playersId, 0);
+			game.forcedMove(newField, playersId, specialMovesId, ownTurn);
 			
-			/*
-			Board ownBoard = game.getBoard();
-			System.out.print(xSize);
-			System.out.print('\n');
-			System.out.print(ySize);
-			System.out.print('\n');
-			Piece ownPiece = new Piece();
-			for (int y = 0; y < ySize; ++y) {
-				for (int x = 0; x < xSize; ++x) {
-					ownPiece = ownBoard.getPiece(x, y);
-					PieceType ownType = ownPiece.getPieceType();
-					if (ownType != null) {
-						System.out.print(ownType.getId());
-					}
-					else {
-						System.out.print('-');
-					}
-				}
-				System.out.print('\n');
-			}
-			*/
-		} catch(Exception e) {
-			JOptionPane.showMessageDialog(null, "File not found");
-		}
+		} catch(Exception e) { }
 		assertTrue(true);
     } 	
 	
-	@Ignore
-	@Test
-	public void testForcedMove01() {
-		int xSize = 8;
-		int ySize = 8;
-		int [][] newField = new int[xSize][ySize];
-		newField[0][1] = 6;
-		newField[1][1] = 6;	
-		newField[2][1] = 6;
-		newField[3][1] = 6;	
-		newField[4][1] = 6;
-		newField[5][1] = 6;	
-		newField[6][1] = 6;
-		newField[7][1] = 6;	
-		newField[0][6] = 6;
-		newField[1][6] = 6;	
-		newField[2][6] = 6;
-		newField[3][6] = 6;	
-		newField[4][6] = 6;
-		newField[5][6] = 6;	
-		newField[6][6] = 6;
-		newField[7][6] = 6;	
-		
-		int [][] playersId = new int[xSize][ySize];
-		playersId[0][1] = 2;
-		playersId[1][1] = 2;
-		playersId[2][1] = 2;
-		playersId[3][1] = 2;
-		playersId[4][1] = 2;
-		playersId[5][1] = 2;
-		playersId[6][1] = 2;
-		playersId[7][1] = 2;
-		playersId[0][6] = 1;
-		playersId[1][6] = 1;
-		playersId[2][6] = 1;
-		playersId[3][6] = 1;
-		playersId[4][6] = 1;
-		playersId[5][6] = 1;
-		playersId[6][6] = 1;
-		playersId[7][6] = 1;
-		
-		game = RulesParser.parse("./rules/Chess.xml");
-		game.forcedMove(newField, playersId, 0);
-			//
-		Board ownBoard = game.getBoard();
-		System.out.print(xSize);
-		System.out.print('\n');
-		System.out.print(ySize);
-		System.out.print('\n');
-		Piece ownPiece = new Piece();
-		for (int y = ySize - 1; y > -1; --y) {
-			for (int x = 0; x < xSize; ++x) {
-				ownPiece = ownBoard.getPiece(x, y);
-				PieceType ownType = ownPiece.getPieceType();
-				if (ownType != null) {
-					System.out.print(ownType.getId());
-				}
-				else {
-					System.out.print('-');
-				}
-			}
-			System.out.print('\n');
-		}
-		for (int y = ySize - 1; y > -1; --y) {
-			for (int x = 0; x < xSize; ++x) {
-				ownPiece = ownBoard.getPiece(x, y);
-				if (ownPiece != null) {
-					if (ownPiece.getPlayer() != null) {
-						System.out.print(ownPiece.getPlayer().getId());
-					} else {
-						System.out.print('-');
-					}
-				} else {
-					System.out.print('-');
-				}
-			}
-			System.out.print('\n');
-		}
-		assertTrue(true);
-	}
 }
 
